b454107c6ffab8d88444b094f4ba911d2bd5cdf1
Kasmik004 <67088222+Kasmik004@users.noreply.github.com>
2024-06-19 10:51:57 +0545

    Feat: Create impls for g-m

--- src/graph.cpp
+++ src/graph.cpp
@@ -128,4 +128,65 @@ public:
     {
         return adjacency_list->size();
     }
+
+    int numEdges()
+    {
+        int count = 0;
+        for (int i = 0; i < adjacency_list->size(); i++)
+        {
+            count += adjacency_list->at(i).second.size();
+        }
+        return count;
+    };
+    int indegree(int v)
+    {
+        int count = 0;
+        for (int i = 0; i < adjacency_list->size(); i++)
+        {
+            for (int j = 0; j < adjacency_list->at(i).second.size(); j++)
+            {
+                if (adjacency_list->at(i).second[j] == v)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    };
+    int outdegree(int v)
+    {
+        int count = 0 for (int i = 0; i < adjacency_list->size(); i++)
+        {
+            if (adjacency_list->at(i).first() == v)
+            {
+                count += adjacency_list->at(i).second.size();
+            }
+        }
+
+        return adjacency_list->at(v).second.size();
+    };
+    int degree(int v)
+    {
+        return adjacency_list->at(v).second.size() + indegree(v);
+    };
+    void neighbors(int v)
+    {
+        for (int i = 0; i < adjacency_list->at(v).second.size(); i++)
+        {
+            std::cout << adjacency_list->at(v).second[i] << " ";
+        }
+        std::cout << std::endl;
+    };
+    void checkNeighbors(int v, int w)
+    {
+        for (int i = 0; i < adjacency_list->at(v).second.size(); i++)
+        {
+            if (adjacency_list->at(v).second[i] == w)
+            {
+                std::cout << "True" << std::endl;
+                return;
+            }
+        }
+        std::cout << "False" << std::endl;
+    };
 };
